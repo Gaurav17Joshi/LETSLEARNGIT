d58781d763a9ee6074ef30ea5f795932da098806
Gaurav Joshi <gauravcjoshi17@gmail.com>
2022-10-13 17:32:32 +0530

    adding diffgit.cpp

---
+++ diffgit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter 2 numbers: ";
+    int a,b;
+    cin>>a>>b;
+    cout<<"The differnce is: ";
+    cout<<a+b;
+    return 0;
+}
